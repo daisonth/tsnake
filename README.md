194edf9cfa07306690ac7201775758f9f7bdea3a
Daison <daisonthms@gmail.com>
2023-05-21 07:39:32 +0530

    update

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 <div align="center">
 <h1>tsnake</h1>
 
-C++ Terminal Snake game using Ncurses library.
+C++ Terminal Snake game made using Ncurses library.
+
+<h4>Instalation</h4>
+```cpp
+c++ -0 tsnake tsnake.cpp -lncurses
+./tsnake
+```
 
 <img src="screenshots/scr0.png" alt="Preview" />
 <img src="screenshots/scr1.png" alt="Preview" />
--- tsnake.cpp
+++ tsnake.cpp
@@ -1,152 +1,163 @@
-#include <ncurses.h>
-#include <string>
-#include <cstdlib>
-#include <unistd.h>
-#include <deque>
-#include <random>
 #include <algorithm>
+#include <cstdlib>
+#include <deque>
+#include <ncurses.h>
+#include <random>
+#include <string>
+#include <unistd.h>
+
 #include "tsnake.hpp"
 
 #define height 25
 #define width (height * 2)
 
-int main(int argc, char *argv[])
-{
-	std::random_device rd;
-	std::uniform_int_distribution<int> randX(1,width-3);
-	std::uniform_int_distribution<int> randY(1,height-3);
+int main(int argc, char *argv[]) {
+  std::random_device rd;
+  std::uniform_int_distribution<int> randX(1, width - 3);
+  std::uniform_int_distribution<int> randY(1, height - 3);
 
-	initscr();
-	curs_set(0); //hide terminal cursor
-	cbreak(); // read individual keY hits rather than complete lines of text
-	noecho(); // stop echoing keYboard inputs
-	refresh(); // refresh stdscr
+  initscr();
+  curs_set(0); // hide terminal cursor
+  cbreak();    // read individual keY hits rather than complete lines of text
+  noecho();    // stop echoing keYboard inputs
+  refresh();   // refresh stdscr
 
-	WINDOW *board = createWin();
+  WINDOW *board = createWin();
 
-	int input;
-	int highscore = 0;
+  int input;
+  int highscore = 0;
 
-	do
-	{
-		int X = width/2, Y = height/2;
-		int tailY = Y, tailX = X;
-		int appleX = randX(rd), appleY = randY(rd);
-		int score = 0;
-		int nextdireciton = -1, direction = KEY_UP;
-		char body = 'o', apple = 'A';
+  do {
+    int X = width / 2, Y = height / 2;
+    int tailY = Y, tailX = X;
+    int appleX = randX(rd), appleY = randY(rd);
+    int score = 0;
+    int nextdireciton = -1, direction = KEY_UP;
+    char body = 'o', apple = 'A';
 
-		std::vector<char> snake(10,'X');
-		std::deque<int> moves;
-		std::vector<std::pair<int,int>> positons;
+    std::vector<char> snake(10, 'X');
+    std::deque<int> moves;
+    std::vector<std::pair<int, int>> positons;
 
-		printHomeScreen(board, highscore);
+    printHomeScreen(board, highscore);
 
-		wgetch(board);
+    wgetch(board);
 
-		redraw(board);
+    redraw(board);
 
-		printScore(board, score);
+    printScore(board, score);
 
-		// print snake
-		for(char c: snake)
-		{
-			mvwaddch(board, tailY++, tailX, body);
-			positons.push_back(std::make_pair(tailX,tailY));
-		}
-		wrefresh(board);
+    // print snake
+    for (char c : snake) {
+      mvwaddch(board, tailY++, tailX, body);
+      positons.push_back(std::make_pair(tailX, tailY));
+    }
+    wrefresh(board);
 
-		// print the first apple
-		mvwaddch(board,appleY,appleX,apple);
+    // print the first apple
+    mvwaddch(board, appleY, appleX, apple);
 
-		for(int i=0; i<snake.size(); i++) moves.push_back(KEY_UP); //record initial moves for tail end
+    for (int i = 0; i < snake.size(); i++)
+      moves.push_back(KEY_UP); // record initial moves for tail end
 
-		auto pos = std::find(positons.begin(), positons.end(), std::make_pair(tailX,tailY));
-		auto it = std::find(positons.begin(), positons.end(), std::make_pair(appleX,appleY));
-		mvwaddch(board,tailY,tailX,' ');
+    auto pos = std::find(positons.begin(), positons.end(),
+                         std::make_pair(tailX, tailY));
+    auto it = std::find(positons.begin(), positons.end(),
+                        std::make_pair(appleX, appleY));
+    mvwaddch(board, tailY, tailX, ' ');
 
-		while(Y < height-1 && Y > 0 && X < width-1 && X > 0 )  // main game loop
-		{
-			// print new head positon
-			mvwaddch(board,Y,X,body);
-			positons.push_back(std::make_pair(X,Y));
-			wrefresh(board);
+    while (Y < height - 1 && Y > 0 && X < width - 1 && X > 0) // main game loop
+    {
+      // print new head positon
+      mvwaddch(board, Y, X, body);
+      positons.push_back(std::make_pair(X, Y));
+      wrefresh(board);
 
-			// wait and take input
-			do {
-				wtimeout(board, 200);
-				nextdireciton = wgetch(board);
-			} while(nextdireciton != KEY_UP && nextdireciton != KEY_DOWN && nextdireciton != KEY_LEFT && nextdireciton != KEY_RIGHT && nextdireciton != -1);
+      // wait and take input
+      do {
+        wtimeout(board, 200);
+        nextdireciton = wgetch(board);
+      } while (nextdireciton != KEY_UP && nextdireciton != KEY_DOWN &&
+               nextdireciton != KEY_LEFT && nextdireciton != KEY_RIGHT &&
+               nextdireciton != -1);
 
-			// if there is change in direction
-			if(nextdireciton  != -1)
-			{
-				if(
-				    (direction == KEY_RIGHT && nextdireciton != KEY_LEFT) || \
-				    (direction == KEY_LEFT && nextdireciton != KEY_RIGHT) || \
-				    (direction == KEY_DOWN && nextdireciton != KEY_UP) || \
-				    (direction == KEY_UP && nextdireciton != KEY_DOWN)
-				)
-					direction = nextdireciton;
-			}
+      // if there is change in direction
+      if (nextdireciton != -1) {
+        if ((direction == KEY_RIGHT && nextdireciton != KEY_LEFT) ||
+            (direction == KEY_LEFT && nextdireciton != KEY_RIGHT) ||
+            (direction == KEY_DOWN && nextdireciton != KEY_UP) ||
+            (direction == KEY_UP && nextdireciton != KEY_DOWN))
+          direction = nextdireciton;
+      }
 
-			// updtae next head direction from input
-			if(direction == KEY_UP) --Y;
-			else if(direction == KEY_DOWN) ++Y;
-			else if(direction == KEY_LEFT) --X;
-			else if(direction == KEY_RIGHT) ++X;
+      // updtae next head direction from input
+      if (direction == KEY_UP)
+        --Y;
+      else if (direction == KEY_DOWN)
+        ++Y;
+      else if (direction == KEY_LEFT)
+        --X;
+      else if (direction == KEY_RIGHT)
+        ++X;
 
-			// if the snake eat the apple
-			if(Y == appleY && X == appleX)
-			{
-				// increase score bY 5 and print it
-				score+=5;
-				printScore(board, score);
+      // if the snake eat the apple
+      if (Y == appleY && X == appleX) {
+        // increase score bY 5 and print it
+        score += 5;
+        printScore(board, score);
 
-				// print new apple in another random location
-				do {
-					appleY = randY(rd);
-					appleX = randX(rd);
-					mvwaddch(board,appleY,appleX,apple);
-					auto it = std::find(positons.begin(), positons.end(), std::make_pair(appleX,appleY));
-				} while(it == positons.end()); // new apple location must note be inside the snake body
+        // print new apple in another random location
+        do {
+          appleY = randY(rd);
+          appleX = randX(rd);
+          mvwaddch(board, appleY, appleX, apple);
+          auto it = std::find(positons.begin(), positons.end(),
+                              std::make_pair(appleX, appleY));
+        } while (it == positons.end()); // new apple location must note be
+                                        // inside the snake body
 
-				moves.push_back(direction); //record the next move
-			}
-			else
-			{
-				moves.push_back(direction); //record the next move
+        moves.push_back(direction); // record the next move
+      } else {
+        moves.push_back(direction); // record the next move
 
-				// update tail end
-				if(moves.front() == KEY_UP) --tailY;
-				else if(moves.front() == KEY_DOWN) ++tailY;
-				else if(moves.front() == KEY_LEFT) --tailX;
-				else if(moves.front() == KEY_RIGHT) ++tailX;
+        // update tail end
+        if (moves.front() == KEY_UP)
+          --tailY;
+        else if (moves.front() == KEY_DOWN)
+          ++tailY;
+        else if (moves.front() == KEY_LEFT)
+          --tailX;
+        else if (moves.front() == KEY_RIGHT)
+          ++tailX;
 
-				// remove the current tail end piece
-				moves.pop_front();
-				positons.erase(positons.begin());
-				mvwaddch(board,tailY,tailX,' ');
-			}
+        // remove the current tail end piece
+        moves.pop_front();
+        positons.erase(positons.begin());
+        mvwaddch(board, tailY, tailX, ' ');
+      }
 
-			// check if the snake bite itself
-			auto pos = std::find(positons.begin(), positons.end(), std::make_pair(X,Y));
-			if(pos != positons.end()) break;
-		}
+      // check if the snake bite itself
+      auto pos =
+          std::find(positons.begin(), positons.end(), std::make_pair(X, Y));
+      if (pos != positons.end())
+        break;
+    }
 
-		if(highscore < score) highscore = score;
-		printEndScreen(board, highscore, score);
+    if (highscore < score)
+      highscore = score;
+    printEndScreen(board, highscore, score);
 
-		// take input
-		do {
-			wtimeout(board,-1);
-			input = wgetch(board);
-		} while(input != 10 && input != 113); // input must be either 'ENTER' or 'Q';
+    // take input
+    do {
+      wtimeout(board, -1);
+      input = wgetch(board);
+    } while (input != 10 &&
+             input != 113); // input must be either 'ENTER' or 'Q';
 
-	} while(input == 10);
+  } while (input == 10);
 
-	destroy_win(board);
+  destroy_win(board);
 
-	endwin();
-	return 0;
+  endwin();
+  return 0;
 }
